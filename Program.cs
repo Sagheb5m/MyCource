22801e1a62c002f5fe4d13fc70b25d91146687dd
Sagheb5m <104869978+Sagheb5m@users.noreply.github.com>
2024-03-11 08:40:30 +0330

    Add System Class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-
+using System;
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
 int num1 = Console.ReadLine();
 int num2 = Console.ReadLine();
 Console.WriteLine("Additional:"+(num1+num2));
+    }
+  }
+}
